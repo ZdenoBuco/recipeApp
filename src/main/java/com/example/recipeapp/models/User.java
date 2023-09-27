31da76755b2366dd3374fe54c9e298d4f0780489
Zdenko Bučo <buco.zdeno@gmail.com>
2023-09-27 19:21:53 +0200

    add User entity

--- src/main/java/com/example/recipeapp/models/User.java
+++ src/main/java/com/example/recipeapp/models/User.java
@@ -1,4 +1,18 @@
 package com.example.recipeapp.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    private String email;
+    private String password;
+    private String fullName;
 }
